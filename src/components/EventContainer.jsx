66ad8c8d2df18c624bd3957300d1d5b3e0087f7f
Tejveer Singh <tejveer8826@gmail.com>
2024-09-15 21:18:14 +0530

    2nd commit

--- src/components/EventContainer.jsx
+++ src/components/EventContainer.jsx
@@ -4,8 +4,12 @@ const EventContainer = ({ colors, dir }) => {
   return (
     <div className={`flex  ${dir} mt-36`}>
       {/* Marquee Section */}
-      <div className="w-full md:w-1/2 text-center   ml-0 md:ml-20 overflow-hidden relative z-20">
-        <div className={`absolute inset-0  flex animate-marquee`}>
+      <div className="w-full md:w-1/2 text-center ml-0 overflow-hidden relative z-20">
+        {/* Left and right shadows */}
+        <div className="absolute top-0 left-0 h-full w-10 bg-gradient-to-r from-gray-900 opacity-50 pointer-events-none z-30"></div>
+        <div className="absolute top-0 right-0 h-full w-10 bg-gradient-to-l from-gray-900 opacity-50 pointer-events-none z-30"></div>
+
+        <div className={`absolute inset-0 flex animate-marquee`}>
           <div className="flex space-x-8 my-auto z-0" style={{ width: '100%' }}>
             {colors.map((color, index) => (
               <div
@@ -17,6 +21,7 @@ const EventContainer = ({ colors, dir }) => {
           </div>
         </div>
       </div>
+
 
       {/* Event Details Section */}
       <div className="w-full md:w-1/2 py-20 px-8 md:px-16 mx-auto flex items-center justify-center  text-slate-50 font-normal text-xl opacity-85 tracking-widest">
